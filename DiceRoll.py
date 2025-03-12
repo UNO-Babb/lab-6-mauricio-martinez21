6f59d496557d6a8c9b90056c4873a9f254ba05d1
mauricio-martinez21 <mmartinez34@unomaha.edu>
2025-03-12 14:18:02 +0000

    done

--- DiceRoll.py
+++ DiceRoll.py
@@ -30,3 +30,73 @@ def main():
     #print(r)
 if __name__ == '__main__':
   main()
+
+#WordCount.py
+#Name:mauricio martinez
+#Date:02/26/25
+#Assignment:Lab 6 
+
+def main():
+  textFile = open("fish.txt", 'r')
+  lineCount=0
+  wordCount=0
+  letterCount=0
+
+
+  
+  for line in textFile:
+    lineCount+=1
+    print(line)
+    words=line.split()
+    
+    for word in words:
+      wordCount+=1
+      print("Lines:",lineCount)
+      print("word:wordCount")
+      words=line.split()
+      
+      for letter in word:
+        print(letter)
+  
+  print("lines:",lineCount)
+
+if __name__ == '__main__':
+  main()
+
+  #WordIndex.py
+#Name:Mauricio Martinez
+#Date:02/26/25
+#Assignment:Lab 6
+
+def main():
+  textFile = open ("gettysberg.txt",'r')
+                  
+  wordList = {} #create an empty dictionary
+
+  LineNum=0
+  for line in textFile:
+    LineNum = LineNum + 1
+    words=line.split()
+    for word in words:
+      word=word.lower()
+      word=word.replace(",","")
+      word=word.replace(".","")
+      word=word.replace("\n","")
+      word=word.replace("!","")
+    
+      if word in wordList:
+        wordList[word].append(LineNum) #add to existing entry 
+      else:
+        wordList[word]=[LineNum] #add word to list
+
+  print(wordList)
+  for word in wordList:
+    print(word,wordList[word])
+    
+    
+ 
+
+
+
+if __name__ == '__main__':
+  main()
